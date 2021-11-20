2882ed57f562224724ccad8ddfbade35d82180c0
Maclay Ramsey <maclayramsey@ufl.edu>
2021-11-20 01:16:04 -0500

    update

--- scripts/firstmap.js
+++ scripts/firstmap.js
@@ -9,22 +9,15 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: 'pk.eyJ1IjoibWFjbGF5ciIsImEiOiJja3czcmR2MGowOWh0Mm9vODBuY3A0MDgyIn0.MSVeSEwFEnuEEI7Z3pO4Ew'
 }).addTo(mymap);
 
-var circle = L.circle([35.20748385793186, -80.86549126476281], {
-    color: 'purple',
-    fillColor: 'purple',
-    fillOpacity: 0.5,
-    radius: 160.934
-}).addTo(mymap);
-
 var marker1 = L.marker([35.20748385793186, -80.86549126476281]).addTo(mymap);
 var marker2 = L.marker([35.21339325599912, -80.85853130779581]).addTo(mymap);
 var marker3 = L.marker([35.21186224084088, -80.86150057981177]).addTo(mymap);
 var marker4 = L.marker([35.20109164233384, -80.86711222340199]).addTo(mymap);
 
 
-marker1.bindPopup("<b>Hub South End Apartments</b><br>$1,460");
-marker2.bindPopup("<b>Camden Gallery Apartments</b><br>$1,460");
-marker3.bindPopup("<b>Hawk Apartments</b><br>$1,460");
-marker4.bindPopup("<b>MAA Reserve</b><br>$1,460");
+marker1.bindPopup("<b>Hub South End Apartments</b>");
+marker2.bindPopup("<b>Camden Gallery Apartments</b>");
+marker3.bindPopup("<b>Hawk Apartments</b>");
+marker4.bindPopup("<b>MAA Reserve</b>");
 
 var popup = L.popup();
