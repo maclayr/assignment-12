d154f85973832b82354fbff33bb056c6ccc81cff
Maclay Ramsey <maclayramsey@ufl.edu>
2021-11-20 01:22:47 -0500

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -9,10 +9,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: 'pk.eyJ1IjoibWFjbGF5ciIsImEiOiJja3czcmR2MGowOWh0Mm9vODBuY3A0MDgyIn0.MSVeSEwFEnuEEI7Z3pO4Ew'
 }).addTo(mymap);
 
+<<<<<<< HEAD:scripts/main.js
 <<<<<<< HEAD:scripts/firstmap.js
 =======
 var polyline = L.polyline(latlngs).addTo(mymap);
 
+=======
+>>>>>>> parent of 2882ed5 (update):scripts/firstmap.js
 var circle = L.circle([35.20748385793186, -80.86549126476281], {
     color: 'purple',
     fillColor: 'purple',
@@ -20,17 +23,20 @@ var circle = L.circle([35.20748385793186, -80.86549126476281], {
     radius: 160.934
 }).addTo(mymap);
 
+<<<<<<< HEAD:scripts/main.js
 >>>>>>> parent of 00eb1e4 (commit):scripts/main.js
+=======
+>>>>>>> parent of 2882ed5 (update):scripts/firstmap.js
 var marker1 = L.marker([35.20748385793186, -80.86549126476281]).addTo(mymap);
 var marker2 = L.marker([35.21339325599912, -80.85853130779581]).addTo(mymap);
 var marker3 = L.marker([35.21186224084088, -80.86150057981177]).addTo(mymap);
 var marker4 = L.marker([35.20109164233384, -80.86711222340199]).addTo(mymap);
 
 
-marker1.bindPopup("<b>Hub South End Apartments</b>");
-marker2.bindPopup("<b>Camden Gallery Apartments</b>");
-marker3.bindPopup("<b>Hawk Apartments</b>");
-marker4.bindPopup("<b>MAA Reserve</b>");
+marker1.bindPopup("<b>Hub South End Apartments</b><br>$1,460");
+marker2.bindPopup("<b>Camden Gallery Apartments</b><br>$1,460");
+marker3.bindPopup("<b>Hawk Apartments</b><br>$1,460");
+marker4.bindPopup("<b>MAA Reserve</b><br>$1,460");
 
 var popup = L.popup();
 
